8f83c3cb8796d4245d97955646547303fcf750e4
Elena Primachova <olena.primachova@gmail.com>
2016-11-03 21:56:06 +0100

    Simulation logic implemented.

--- C#/Airplane_Travelling/Airplane_Travelling/Form2.Designer.cs
+++ C#/Airplane_Travelling/Airplane_Travelling/Form2.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -39,6 +38,9 @@
             this.label7 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -56,14 +58,6 @@
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
             this.dataGridView1.Size = new System.Drawing.Size(467, 116);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(14, 221);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(641, 175);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
             // 
             // label1
             // 
@@ -144,6 +138,7 @@
             this.button1.TabIndex = 9;
             this.button1.Text = "Start simulation";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox1
             // 
@@ -159,18 +154,55 @@
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             // 
+            // textBox1
+            // 
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(15, 222);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(639, 174);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(85, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 33);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "View log file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(427, 408);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 33);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "New simulation";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(667, 453);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form2";
             this.Text = "Airplane Travelling Simulation";
@@ -186,7 +218,6 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -196,5 +227,8 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- C#/Airplane_Travelling/Airplane_Travelling/Form2.cs
+++ C#/Airplane_Travelling/Airplane_Travelling/Form2.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Airplane_Travelling
 {
@@ -16,6 +13,10 @@ namespace Airplane_Travelling
         {
             InitializeComponent();
         }
+
+        public static string logFilePath;
+        public static int simulationTime;
+        public static int terminalStatePlanes;
 
         class Plane
         {
@@ -29,12 +30,36 @@ namespace Airplane_Travelling
             private int fuelLeft;
             private bool isRefueled;
             private bool passedMidAirport;
+            private bool terminalStateReached;
+
             public void Input(string[] data)
             {
                 planeID = Convert.ToInt16(data[0]);
                 capacity = Convert.ToInt16(data[1]);
                 startAirportID = Convert.ToInt16(data[2]);
                 direction = Convert.ToChar(data[3]);
+            }
+
+            //Setters and getters for Plane class
+            public int PlaneID
+            {
+                get { return planeID; }
+                set { planeID = value; }
+            }
+            public int Capacity
+            {
+                get { return capacity; }
+                set { capacity = value; }
+            }
+            public int StartAirportID
+            {
+                get { return startAirportID; }
+                set { startAirportID = value; }
+            }
+            public char Direction
+            {
+                get { return direction; }
+                set { direction = value; }
             }
             public int MidAirportID
             {
@@ -66,6 +91,40 @@ namespace Airplane_Travelling
                 get { return passedMidAirport; }
                 set { passedMidAirport = value; }
             }
+            public bool TerminalStateReached
+            {
+                get { return terminalStateReached; }
+                set { terminalStateReached = value; }
+            }
+            public void DEPARTURE(TextBox output)
+            {
+                output.AppendText(Environment.NewLine + "Plane " + planeID + " departed from airport " + startAirportID);
+                using (StreamWriter log = File.AppendText(logFilePath))
+                {
+                    log.WriteLine(simulationTime.ToString() + "h: Plane " + planeID + " departed from airport " + startAirportID);
+                }
+            }
+            public void ARRIVAL (TextBox output)
+            {
+                terminalStateReached = true;
+                terminalStatePlanes++;
+                output.AppendText(Environment.NewLine + "Plane " + planeID + " arrived to the airport " + destAirportID);
+                using (StreamWriter log = File.AppendText(logFilePath))
+                {
+                    log.WriteLine(simulationTime.ToString() + "h: Plane " + planeID + " arrived to the airport " + destAirportID);
+                }
+            }
+            public void CRASH(TextBox output)
+            {
+                terminalStateReached = true;
+                terminalStatePlanes++;
+                output.AppendText(Environment.NewLine + "Plane " + planeID + " crashed!");
+                using (StreamWriter log = File.AppendText(logFilePath))
+                {
+                    log.WriteLine("<" + simulationTime.ToString() + "h><" + planeID + "><-><-><"+ isRefueled +"><CRASHED>");
+                }
+            }
+
         }
 
         class Airport
@@ -83,25 +142,61 @@ namespace Airplane_Travelling
                 B = data[3];
                 distanceToB = data[4];
             }
-            public int GetAirportID()
+
+            public void REFUEL(Plane p, char from, TextBox output)
             {
-                return airportID;
+                p.PassedMidAirport = true;
+                double percentage = (double)p.FuelLeft/p.Capacity;
+                //if the plane has 50% of fuel or less, refuel it
+                if (percentage <= 0.5)
+                {
+                    p.IsRefueled = true;
+                    p.FuelLeft = p.Capacity;
+                    output.AppendText(Environment.NewLine + "Plane " + p.PlaneID + " is refueled at the airport " + airportID);
+                    using (StreamWriter log = File.AppendText(logFilePath))
+                    {
+                        log.WriteLine("<" + simulationTime.ToString() + "h><" + p.PlaneID + "><" + airportID + "><" + from + "><"+p.IsRefueled+"><REFUELING>");
+                    }
+                }
+                else //otherwise, decline the refueling
+                {
+                    output.AppendText(Environment.NewLine + "Refueling of the plane " + p.PlaneID + " at the airport " + airportID + " is not confirmed.");
+                    using (StreamWriter log = File.AppendText(logFilePath))
+                    {
+                        log.WriteLine("<" + simulationTime.ToString() + "h><" + p.PlaneID + "><" + airportID + "><" + from + "><" + p.IsRefueled + "><PASSED BY>");
+                    }
+                    if (A == p.DestAirportID)
+                        p.HoursLeft = distanceToA;
+                    else
+                        p.HoursLeft = distanceToB;
+                }
             }
-            public int GetA()
+
+            //Setters and getters for Airport class
+            public int AirportID
             {
-                return A;
+                get { return airportID; }
+                set { airportID = value; }
             }
-            public int GetDistanceToA()
+            public int AID
             {
-                return distanceToA;
+                get { return A; }
+                set { A = value; }
             }
-            public int GetB()
+            public int DistanceToA
             {
-                return B;
+                get { return distanceToA; }
+                set { distanceToA = value; }
             }
-            public int GetDistanceToB()
+            public int BID
             {
-                return distanceToB;
+                get { return B; }
+                set { B = value; }
+            }
+            public int DistanceToB
+            {
+                get { return distanceToB; }
+                set { distanceToB = value; }
             }
         }
 
@@ -144,50 +239,162 @@ namespace Airplane_Travelling
             }
 
             //initialization of the flightmap table
+            //(instead of dataGridView these data can be stored in simple matrix)
             foreach (Airport a in airports)
             {
                 //calculation of the entire route in A direction
                 dataGridView1.Rows.Add();
                 int rowIndex = dataGridView1.Rows.Count - 1;
-                dataGridView1.Rows[rowIndex].Cells[0].Value = a.GetAirportID();
+                dataGridView1.Rows[rowIndex].Cells[0].Value = a.AirportID;
                 dataGridView1.Rows[rowIndex].Cells[1].Value = "A";
-                dataGridView1.Rows[rowIndex].Cells[2].Value = a.GetA();
-                dataGridView1.Rows[rowIndex].Cells[3].Value = a.GetDistanceToA();
-                Airport destinationA = airports.Find(x => x.GetAirportID() == a.GetA());
-                if (destinationA.GetA() == a.GetAirportID())
+                dataGridView1.Rows[rowIndex].Cells[2].Value = a.AID;
+                dataGridView1.Rows[rowIndex].Cells[3].Value = a.DistanceToA;
+                Airport destinationA = airports.Find(x => x.AirportID == a.AID);
+                if (destinationA.AID == a.AirportID)
                 {
-                    dataGridView1.Rows[rowIndex].Cells[4].Value = destinationA.GetB();
-                    dataGridView1.Rows[rowIndex].Cells[5].Value = destinationA.GetDistanceToB();
+                    dataGridView1.Rows[rowIndex].Cells[4].Value = destinationA.BID;
+                    dataGridView1.Rows[rowIndex].Cells[5].Value = destinationA.DistanceToB;
                 }
                 else
                 {
-                    dataGridView1.Rows[rowIndex].Cells[4].Value = destinationA.GetA();
-                    dataGridView1.Rows[rowIndex].Cells[5].Value = destinationA.GetDistanceToA();
+                    dataGridView1.Rows[rowIndex].Cells[4].Value = destinationA.AID;
+                    dataGridView1.Rows[rowIndex].Cells[5].Value = destinationA.DistanceToA;
                 }
 
                 //calculation of the entire route in B direction
                 dataGridView1.Rows.Add();
                 rowIndex = dataGridView1.Rows.Count - 1;
-                dataGridView1.Rows[rowIndex].Cells[0].Value = a.GetAirportID();
+                dataGridView1.Rows[rowIndex].Cells[0].Value = a.AirportID;
                 dataGridView1.Rows[rowIndex].Cells[1].Value = "B";
-                dataGridView1.Rows[rowIndex].Cells[2].Value = a.GetB();
-                dataGridView1.Rows[rowIndex].Cells[3].Value = a.GetDistanceToB();
-                Airport destinationB = airports.Find(x => x.GetAirportID() == a.GetB());
-                if (destinationB.GetA() == a.GetAirportID())
+                dataGridView1.Rows[rowIndex].Cells[2].Value = a.BID;
+                dataGridView1.Rows[rowIndex].Cells[3].Value = a.DistanceToB;
+                Airport destinationB = airports.Find(x => x.AirportID == a.BID);
+                if (destinationB.AID == a.AirportID)
                 {
-                    dataGridView1.Rows[rowIndex].Cells[4].Value = destinationB.GetB();
-                    dataGridView1.Rows[rowIndex].Cells[5].Value = destinationB.GetDistanceToB();
+                    dataGridView1.Rows[rowIndex].Cells[4].Value = destinationB.BID;
+                    dataGridView1.Rows[rowIndex].Cells[5].Value = destinationB.DistanceToB;
                 }
                 else
                 {
-                    dataGridView1.Rows[rowIndex].Cells[4].Value = destinationB.GetA();
-                    dataGridView1.Rows[rowIndex].Cells[5].Value = destinationB.GetDistanceToA();
+                    dataGridView1.Rows[rowIndex].Cells[4].Value = destinationB.AID;
+                    dataGridView1.Rows[rowIndex].Cells[5].Value = destinationB.DistanceToA;
                 }
             }
 
             //initialize plane calculated parameters
+            foreach (Plane p in planes)
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (row.Cells[0].Value.ToString().Equals(p.StartAirportID.ToString())
+                        && row.Cells[1].Value.ToString().Equals(p.Direction.ToString()))
+                    {
+                        p.MidAirportID = Convert.ToInt16(row.Cells[2].Value);
+                        p.HoursLeft = Convert.ToInt16(row.Cells[3].Value);
+                        p.DestAirportID = Convert.ToInt16(row.Cells[4].Value);
+                        break;
+                    }
+                }
+                p.FuelLeft = p.Capacity;
+            }
         }
 
         //SIMULATION
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            simulationTime = 0;
+            logFilePath = "log" + DateTime.Now.Ticks + ".txt";
+            terminalStatePlanes = 0;
+
+            using (StreamWriter log = File.CreateText(logFilePath))
+            {
+                log.WriteLine("Airplane travelling simulation started. Current time: "+DateTime.Now);
+                log.WriteLine("Simulation parameters: {0} planes, {1} airports.", Form1.numberOfPlanes, Form1.numberOfAirports);
+                log.WriteLine("Simulation time scale is: 1 hour = 1 second.");
+                log.WriteLine("");
+            }
+
+            textBox1.AppendText("Airplane travelling simulation started. Current time: " + DateTime.Now);
+            textBox1.AppendText(Environment.NewLine + "Simulation parameters: "+ Form1.numberOfPlanes + " planes, "+ Form1.numberOfAirports + " airports.");
+            textBox1.AppendText(Environment.NewLine + "Simulation time scale is: 1 second = 1 hour.");
+            textBox1.AppendText(Environment.NewLine);
+
+            foreach (Plane p in planes)
+            {
+                p.DEPARTURE(textBox1);
+            }
+
+            Thread.Sleep(1000);
+            simulationTime++;
+
+            while (terminalStatePlanes < Form1.numberOfPlanes)
+            {
+                textBox1.AppendText(Environment.NewLine + "Events in " + simulationTime.ToString() + " hour:");
+                foreach (Plane p in planes)
+                {
+                    if (!p.TerminalStateReached) //if the plane is active
+                    {
+                        if (p.IsRefueled && p.HoursLeft == 0) //if the plane was refilled
+                        {
+                            Airport fuelStation = airports.Find(x => x.AirportID == p.MidAirportID);
+                            //it departs from fuel station and continues the flight
+                            if (fuelStation.AID == p.DestAirportID)
+                                p.HoursLeft = fuelStation.DistanceToA;
+                            else
+                                p.HoursLeft = fuelStation.DistanceToB;
+                        }
+                        else
+                        {
+                            //every plane covers 1h of the flight and spends 100 l of fuel
+                            p.HoursLeft = p.HoursLeft - 1;
+                            p.FuelLeft = p.FuelLeft - 100;
+
+                            //if the plane reaches the destination airport
+                            if (p.PassedMidAirport && p.HoursLeft == 0)
+                            {
+                                p.ARRIVAL(textBox1);
+                            }
+
+                            //if the plane reaches the middle airport, it requests refueling
+                            else if (!p.PassedMidAirport && p.HoursLeft == 0)
+                            {
+                                char from;
+                                Airport fuelStation = airports.Find(x => x.AirportID == p.MidAirportID);
+                                if (fuelStation.AID == p.StartAirportID)
+                                    from = 'A';
+                                else
+                                    from = 'B';
+                                //airport decides whether to refuel the plane or no
+                                fuelStation.REFUEL(p, from, textBox1);
+                            }
+
+                            //if the plane runs out of fuel, it crashes
+                            else if (p.FuelLeft == 0)
+                            {
+                                p.CRASH(textBox1);
+                            }
+                        }
+                    }
+                }
+                Thread.Sleep(1000);
+                simulationTime++;
+            }
+            button2.Visible = true;
+            button3.Visible = true;
+            button1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", logFilePath);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //destroy form
+            Dispose(true);
+            this.Dispose();
+        }
     }
 }
